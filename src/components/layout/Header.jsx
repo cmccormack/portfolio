50c74094aec0d0162307aa649c48a012c31c918f
Christopher McCormack <mccormack.christopher@gmail.com>
2017-09-27 19:26:21 -0700

    Initial commit

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,13 @@
+/* eslint no-console: 'off' */
+
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+const Header = () => (
+  <h1>Header<hr /></h1>
+)
+
+export default Header
+// Header.propTypes = {
+//   path: PropTypes.string.isRequired,
+// }
