f405ef938e5aeebca04e4fcf7206c0e4791f461d
cmccormack <mccormack.christopher@gmail.com>
2017-10-01 20:50:56 -0700

    Added materialize CSS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,31 +4,52 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-const App = (props) => {
-  console.log(props)
-  console.log(props.children)
-  console.log(PropTypes)
 
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min'
 
-  return (
-    <div>
-      <h1>App</h1>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-      {props.children}
-    </div>
+import Header from './layout/Header'
 
-  )
+const layout = {
+  header: {
+    // brand: {
+    //   name: 'Chris McCormack',
+    //   position: 'left',
+    // },
+    // list: {
+    //   items: [],
+    //   position: 'right',
+    // },
+  },
+  footer: {
+    title: 'Footer',
+    content: 'Stuff will go here later',
+    links: [
+      { name: 'Link 1', url: '#' },
+    ],
+    copyright: {
+      text: '© 2017 Copyright Chris McCormack',
+      link: { name: 'Home', url: 'https://mackville.net' },
+    },
+
+  },
 }
+
+const App = props => (
+  <div>
+    <Header {...layout.header} />
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+    {props.children}
+  </div>
+
+)
+
 
 App.propTypes = {
   children: PropTypes.element.isRequired,
 }
-
-// App.defaultProps = {
-//   children: null,
-// }
 
 export default App
